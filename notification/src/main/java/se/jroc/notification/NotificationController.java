838e63e0d711c621616aea172609d17d41a77e8b
Jonatan Ruohomäki <jonatan@ekotextil.se>
2022-08-07 18:16:11 +0200

    Packaging Microservices to Runnable Jar / Section Finished

--- notification/src/main/java/se/jroc/notification/NotificationController.java
+++ notification/src/main/java/se/jroc/notification/NotificationController.java
@@ -2,11 +2,7 @@ package se.jroc.notification;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import se.jroc.clients.fraud.FraudCheckResponse;
-import se.jroc.clients.fraud.FraudClient;
 import se.jroc.clients.notification.NotificationRequest;
 
 @Slf4j
